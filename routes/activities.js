d261b20368a44fae63b02cab377ab9e0d9ef90b3
Pak, Steve <stevedpak@gmail.com>
2017-12-13 22:21:47 -0500

    Updated form + create activity redirect.

--- routes/activities.js
+++ routes/activities.js
@@ -46,25 +46,25 @@ router.post("/create", (req, res) => {
     var userID = req.user._id;
     activities.createTotalActivity(userID, vm.actName, vm.actDescription, vm.startTime, vm.endTime, vm.actLocation, vm.actNotes);
     req.flash('success', 'Successfully Created Activity ' + vm.actName + '!');
-    res.redirect("/activity/calendar");
+    res.redirect("/activities/calendar");
 });
 
 router.get("/search", async (req, res) => {
     var userID = req.user._id;
     var body = req.body;
     var ac_list = await activities.get_todays_activity(userID);
-    
+
     var found_list = await activities.find_activities_by_date(body.start_time, userID)
-     res.render("activity/search", {userid: userID, my_list: ac_list, my_list2: ac_list});
+    res.render("activity/search", { userid: userID, my_list: ac_list, my_list2: ac_list });
     //console.log(ac_list); 
 });
 router.get("/search/date", async (req, res) => {
     var userID = req.user._id;
     var body = req.body;
     var ac_list = await activities.get_todays_activity(userID);
-    
+
     var found_list = await activities.find_activities_by_date(body.start_time, userID)
-    res.render("activity/search", {userid: userID, my_list: ac_list, my_list2: ac_list});
+    res.render("activity/search", { userid: userID, my_list: ac_list, my_list2: ac_list });
     //console.log(ac_list);
 });
 
